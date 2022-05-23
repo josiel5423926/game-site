12f97bdf595cbf2e3a37f6838b51375f4415b588
Josiel Souza <josielsouza.dj@gmail.com>
2022-05-23 11:56:54 -0300

    feat: adicionando heroku cli

---
+++ backend/models/DbShemaJogo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const slug = require("slug");
+
+const { Schema } = mongoose;
+
+const DbShemaJogo = new Schema({
+  hash: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    default: function () {
+      return slug(Math.random() + this.hash + Math.random());
+    },
+  },
+  valorMoeda: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("apelidoModeloDbShemaJogo", DbShemaJogo);
